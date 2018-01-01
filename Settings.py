cd52933057891f5535832286136e223e7339dd8b
FinnishForce <finnishforce@gmail.com>
2018-01-01 21:49:00 +0200

    Small cleanup
    Part of small cleanup before

--- Settings.py
+++
@@ -1,8 +0,0 @@
-HOST = "irc.twitch.tv"
-PORT = 6667
-PASS = bot_oauth_here
-IDENT = bot_name_here
-CHANNEL = not_sure_if_this_is_even_used_lol
-steamapikey = steamapikey
-lolapikey = lolapikey
-owner = your_own_twitch_name
